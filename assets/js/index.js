39e262e7a897b1f323f462ea48fabf2862cc98e0
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-04-26 12:07:50 +0100

    dynmically render question to page on start

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,13 +3,50 @@ console.log("this is index.js");
 // target start button
 const startButton = document.getElementById("start-btn");
 
+// target main element
+const mainElement = document.getElementById("main");
+
 // target banner section
 const bannerSection = document.getElementById("banner");
 
 // function to render question to page
 const renderQuestion = () => {
   console.log("render question");
+
+  //Create section
+  const section = document.createElement("section");
+  section.setAttribute("class", "content-section question-container");
+
+  //create h2
+  const h2 = document.createElement("h2");
+  h2.setAttribute("class", "content-section question-container");
+  h2.textContent =
+    "1. Do you like the spiderman movies from the Marvel franchise?";
+
+  //create ul and append 3 li
+  const ul = document.createElement("ul");
+  ul.setAttribute("class", "feedback-list");
 };
+
+const li1 = document.createElement("li");
+li1.setAttribute("class", "list-item");
+li1.textContent = "Yes";
+
+const li2 = document.createElement("li");
+li2.setAttribute("class", "list-item");
+li2.textContent = "No";
+
+const li3 = document.createElement("li");
+li3.setAttribute("class", "list-item");
+li3.textContent = "Maybe";
+
+ul.append(li1, li2, li3);
+
+// append h2 and ul to section
+section.append(h2, ul);
+
+// append question section to main element
+mainElement.append(section);
 
 // function to remove banner from page
 const removeBanner = () => {
