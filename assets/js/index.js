72933d0858e7caac3b18e24f2f2c145851715004
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-04-26 12:38:47 +0100

    store answer in local storage

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,47 +6,159 @@ const startButton = document.getElementById("start-btn");
 // target main element
 const mainElement = document.getElementById("main");
 
+// current question index
+let questionIndex = 0;
+
+// all options
+const options = ["Yes", "No", "Maybe"];
+
+// all questions array
+const questions = [
+  {
+    text: "Would you watch the movie again?",
+    options,
+  },
+  {
+    text: "Would you like to see a prequel to this Spider-Man series?",
+    options,
+  },
+  {
+    text: "Would you like to watch Venom and Spider-Man battle in the next movie?",
+    options,
+  },
+  {
+    text: "Would you like to watch Miles Morales and Spider-Man in the next movie?",
+    options,
+  },
+  {
+    text: "Would you like to watch a Spider-Man series on just Miles Morales?",
+    options,
+  },
+  {
+    text: "Would you like watching an animated series on Spider-Man?",
+    options,
+  },
+  {
+    text: "Would you like a new sequel to be released in the future?",
+    options,
+  },
+  {
+    text: "Would you recommend watching the movie to friends and family?",
+    options,
+  },
+];
+
 // target banner section
 const bannerSection = document.getElementById("banner");
+
+// event handler function to handle click events in question section
+const handleOptionClick = (event) => {
+  console.log("clicked somewhere in question section");
+
+  // get current target - where event listener attached to
+  const currentTarget = event.currentTarget;
+
+  // get target - where event initiated or triggered
+  const target = event.target;
+
+  // check if click originates from li only
+  // check if target element is li element
+  if (target.tagName === "LI") {
+    // get the option the user clicked on
+    const value = target.getAttribute("data-value");
+
+    // get the question the user answered
+    const question = questions[questionIndex].text;
+
+    // build an answer object that contains question and answer
+    const answer = {
+      question,
+      value,
+    };
+
+    // store answer in local storage
+    storeAnswerInLS(answer);
+
+    // remove question
+    removeQuestion();
+
+    if (questionIndex < questions.length - 1) {
+      // go to next question if not the last question
+      // increment the question index by 1
+      questionIndex += 1;
+
+      // render question
+      renderQuestion();
+    } else {
+      // if last question then render results and form
+      renderResults();
+      renderForm();
+    }
+  }
+};
+
+// function to render the results
+const renderResults = () => {
+  console.log("render results");
+};
+
+// function to render the form
+const renderForm = () => {
+  console.log("render form");
+};
 
 // function to render question to page
 const renderQuestion = () => {
   console.log("render question");
 
+  // get current question
+  const currentQuestion = questions[questionIndex];
+
   //Create section
   const section = document.createElement("section");
   section.setAttribute("class", "content-section question-container");
+  section.setAttribute("id", "question-container");
 
   //create h2
   const h2 = document.createElement("h2");
   h2.setAttribute("class", "content-section question-container");
-  h2.textContent =
-    "1. Do you like the spiderman movies from the Marvel franchise?";
+  // h2.textContent =
+  //   "1. Do you like the spiderman movies from the Marvel franchise?";
+  h2.textContent = `${questionIndex + 1}. ${currentQuestion.text}`;
 
   //create ul and append 3 li
   const ul = document.createElement("ul");
   ul.setAttribute("class", "feedback-list");
+
+  const li1 = document.createElement("li");
+  li1.setAttribute("class", "list-item");
+  li1.setAttribute("data-value", currentQuestion.options[0]);
+  // li1.textContent = "Yes";
+  li1.textContent = currentQuestion.options[0];
+
+  const li2 = document.createElement("li");
+  li2.setAttribute("class", "list-item");
+  li2.setAttribute("data-value", currentQuestion.options[1]);
+  // li2.textContent = "No";
+  li2.textContent = currentQuestion.options[1];
+
+  const li3 = document.createElement("li");
+  li3.setAttribute("class", "list-item");
+  li3.setAttribute("data-value", currentQuestion.options[2]);
+  // li3.textContent = "Maybe";
+  li3.textContent = currentQuestion.options[2];
+
+  ul.append(li1, li2, li3);
+
+  // append h2 and ul to section
+  section.append(h2, ul);
+
+  // append question section to main element
+  mainElement.append(section);
+
+  // add event listener on question section
+  section.addEventListener("click", handleOptionClick);
 };
-
-const li1 = document.createElement("li");
-li1.setAttribute("class", "list-item");
-li1.textContent = "Yes";
-
-const li2 = document.createElement("li");
-li2.setAttribute("class", "list-item");
-li2.textContent = "No";
-
-const li3 = document.createElement("li");
-li3.setAttribute("class", "list-item");
-li3.textContent = "Maybe";
-
-ul.append(li1, li2, li3);
-
-// append h2 and ul to section
-section.append(h2, ul);
-
-// append question section to main element
-mainElement.append(section);
 
 // function to remove banner from page
 const removeBanner = () => {
@@ -54,9 +166,40 @@ const removeBanner = () => {
   bannerSection.remove();
 };
 
+// function to remove question section from page
+const removeQuestion = () => {
+  console.log("remove question");
+  document.getElementById("question-container").remove();
+};
+const initialiseLocalStorage = () => {
+  // get feedbackResults from LS
+  const feedbackResultsFromLS = JSON.parse(
+    localStorage.getItem("feedbackResults")
+  );
+
+  if (!feedbackResultsFromLS) {
+    // if not exist set LS to have feedbackResults as an empty array
+    localStorage.setItem("feedbackResults", JSON.stringify([]));
+  }
+};
+
+const storeAnswerInLS = (answer) => {
+  // get feedbackResults from LS
+  const feedbackResults = JSON.parse(localStorage.getItem("feedbackResults"));
+
+  // push answer in to array
+  feedbackResults.push(answer);
+
+  // set feedbackResults in LS
+  localStorage.setItem("feedbackResults", JSON.stringify(feedbackResults));
+};
+
 // declare the event handler function for start button click
 const handleStartButtonClick = () => {
   console.log("start button clicked");
+
+  // initialise local storage
+  initialiseLocalStorage();
 
   // remove banner section
   removeBanner();
